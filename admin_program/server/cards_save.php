4f20a1161c5f30ff21a39c6153ff470082a44060
nata315 <Natalia.sasuk@mail.ru>
2026-03-05 16:51:51 +0300

    исправление админки

--- admin_program/server/cards_save.php
+++ admin_program/server/cards_save.php
@@ -1,20 +1,32 @@
 <?php
     $data = [
-        "id" => $_POST["id_card"] ?? '',
-        "name" => $_POST["name_card"] ?? '',
+        "id" => $_POST["id_card"] 
+        "name" => $_POST["name_card"] 
         "description" => $_POST["description_card"] ?? '',
-        "image" => $_POST["image_card"] ?? ''
+        "image" => $_POST["image_card"] 
     ];
+    $com = [
+        "id" => $_POST["id_card"] ,
+        "data" = []
+    ]
 
-    $fileName1 = "images.json"; // убрал слеш в начале
+    $fileName1 = "images.json"; 
+    $fileName2 = "comments.json";
     if (file_exists($fileName1)) {
-        $existingData = json_decode(file_get_contents($fileName1), true) ?? [];
+        $existingData1 = json_decode(file_get_contents($fileName1), true) ?? [];
     } else {
-        $existingData = [];
+        $existingData1 = [];
+    }
+    if (file_exists($fileName2)) {
+        $existingData2 = json_decode(file_get_contents($fileName2), true) ?? [];
+    } else {
+        $existingData2 = [];
     }
     
-    $existingData[] = $data;
-    file_put_contents($fileName1, json_encode($existingData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    $existingData1[] = $data;
+    file_put_contents($fileName1, json_encode($existingData1, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    $existingData2[] = $com;
+    file_put_contents($fileName2, json_encode($existingData2, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     
     exit;
 ?>
